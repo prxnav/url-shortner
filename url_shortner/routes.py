22f6977949651bb5f7b1f36a69c33aaadf84a7de
Pranav Prakash Menon <ppmenon02@gmail.com>
2021-06-04 10:13:22 +0530

    added routes

--- url_shortner/routes.py
+++ url_shortner/routes.py
@@ -1,3 +1,23 @@
 from flask import Blueprint
 
 short = Blueprint("short", __name__)
+
+
+@short.route("/<short_url>")
+def redirect_to_url(short_url):
+    pass
+
+
+@short.route("/")
+def index():
+    pass
+
+
+@short.route("/add_link", methods=["POST"])
+def add_link():
+    pass
+
+
+@short.stats("/stats")
+def stats():
+    pass
